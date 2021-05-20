6f93bb19b26d43adb342b7b381ddb5caaf97485d
Korshunov Egor <egor.dezreng@gmail.com>
2021-05-20 17:41:00 +0300

    refactoring: add DB and refactoring code

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
+var game = require('./controllers/gamecontroller');
+const { PORT } = require('./common/config');
 
+app.use(express.json());
 
-db.sync();
-app.use(require('body-parser'));
+app.use(async (req, res, next) => {
+	await db.sequelize.sync();
+	next();
+})
+
 app.use('/api/auth', user);
+
 app.use(require('./middleware/validate-session'))
+
 app.use('/api/game', game);
-app.listen(function() {
+
+app.listen(PORT, function() {
     console.log("App is listening on 4000");
 })
--- db.js
+++ db.js
@@ -1,19 +1,28 @@
 const Sequelize = require('sequelize');
 const { USER_NAME, PASSWORD } = require('./common/config');
                                 //database username   password
-
 const sequelize = new Sequelize('gamedb', USER_NAME, PASSWORD, {
     host: 'localhost',
+		port: 5433,
     dialect: 'postgres'
-})
+});
 
 sequelize.authenticate().then(
     function success() {
         console.log("Connected to DB1");
-				console.log(USER_NAME, PASSWORD)
     },
 
     function fail(err) {
-        console.log(`Error: ${err}`);
+        console.log(`Error: ${err}`, USER_NAME, PASSWORD);
     }
-)
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.User = require('./models/user')(sequelize, Sequelize);
+db.Game = require('./models/game')(sequelize, Sequelize);
+
+
+module.exports = db;
